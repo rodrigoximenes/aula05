5002429dfc074793c4e4bd78b0ee356f2798e8e9
Rodrigo <rodrig.ximenes@gmail.com>
2025-03-13 21:20:50 -0300

    Final aula 05

--- projeto/todos/views.py
+++ projeto/todos/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import Todo
+from django.views.generic import ListView,CreateView
+from django.urls import reverse_lazy
+
+class TodoListView(ListView):
+    model = Todo;
 
 
-def todo_list(request):
-    todos_lista  = Todo.objects.all() 
-    return render(request, "todos/todo_list.html", {"todos": todos_lista});
+class TodoCreateView(CreateView):
+    model = Todo;
+    fields = ["title", "deadline"];
+    success_url = reverse_lazy("todo_list");
